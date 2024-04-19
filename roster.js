df65e311731878307f57fa38f5b7fc730b0a2fe4
Danny Wong <wong.danny29@gmail.com>
2024-04-18 21:06:19 -0700

    Create roster.js

---
+++ roster.js
@@ -0,0 +1,257 @@
+const roster = [
+  {
+    name: "LISA_LOST", 
+    brand: "SD",
+    gender: "F" 
+  }, 
+  {
+    name: "JUNGO", 
+    brand: "SD", 
+    gender: "F"
+  },
+  {
+    name: "TERESA_SHARP", 
+    brand: "SD",
+    gender: "F" 
+  }, 
+  {
+    name: "SLOANE", 
+    brand: "SD", 
+    gender: "F"
+  },
+  {
+    name: "LAUREN_SNOW", 
+    brand: "SD",
+    gender: "F" 
+  }, 
+  {
+    name: "JANE_MACK", 
+    brand: "SD", 
+    gender: "F"
+  },
+    {
+    name: "LIV_MORGAN", 
+    brand: "SD",
+    gender: "F" 
+  }, 
+  {
+    name: "GIGI_DOLIN", 
+    brand: "SD", 
+    gender: "F"
+  },
+    {
+    name: "MICHELLE_VELVET", 
+    brand: "SD",
+    gender: "F" 
+  }, 
+  {
+    name: "CORA_JADE", 
+    brand: "SD", 
+    gender: "F"
+  },
+    {
+    name: "ROXANNE_PEREZ", 
+    brand: "SD",
+    gender: "F" 
+  }, 
+  {
+    name: "SLADER", 
+    brand: "NXT", 
+    gender: "F"
+  },
+  {
+    name: "NORI", 
+    brand: "NXT", 
+    gender: "F"
+  },
+  {
+    name: "NELLY_STORM", 
+    brand: "NXT", 
+    gender: "F"
+  },
+    {
+    name: "BLAIRE_COUNTY", 
+    brand: "NXT", 
+    gender: "F"
+  },
+    {
+    name: "UPSKIRT_KIRA", 
+    brand: "NXT", 
+    gender: "F"
+  },
+    {
+    name: "BELINDA_LEE", 
+    brand: "NXT", 
+    gender: "F"
+  },
+    {
+    name: "NATASHA_IVY", 
+    brand: "NXT", 
+    gender: "F"
+  },
+    {
+    name: "AMA_BIG", 
+    brand: "RAW", 
+    gender: "F"
+  },
+    {
+    name: "CYNTHIA_RAMIREZ", 
+    brand: "RAW", 
+    gender: "F"
+  },
+      {
+    name: "HALIE_QUINN", 
+    brand: "RAW", 
+    gender: "F"
+  },
+      {
+    name: "JESSICA_CUP", 
+    brand: "RAW", 
+    gender: "F"
+  },
+      {
+    name: "JULIET_SUNSET", 
+    brand: "RAW", 
+    gender: "F"
+  },
+      {
+    name: "MASK_GIRL", 
+    brand: "RAW", 
+    gender: "F"
+  },
+      {
+    name: "ALICIA_HOUSE", 
+    brand: "RAW", 
+    gender: "F"
+  },
+      {
+    name: "JANINE", 
+    brand: "RAW", 
+    gender: "F"
+  },
+      {
+    name: "CARMELLA", 
+    brand: "RAW", 
+    gender: "F"
+  },
+      {
+    name: "NAOMI", 
+    brand: "RAW", 
+    gender: "F"
+  },
+      {
+    name: "TAMINA", 
+    brand: "RAW", 
+    gender: "F"
+  },
+      {
+    name: "CALLUM_WOLF", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "TYSON_EVERETT", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "MR_ENVIRONMENT", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "BEN_SEED", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "KRIPPLER", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "CLIFF_LEMON", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "POSI", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "JASON_HONEY", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "RUSSELL_RUSH", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "JOHNNY_AMERICA", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "BRETT_HUNTER", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "BRENT_HUNTER", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "CLIP_MAVERICK", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "FRED_CHATTY", 
+    brand: "RAW", 
+    gender: "M"
+  },
+        {
+    name: "TURBO", 
+    brand: "NXT", 
+    gender: "M"
+  },
+          {
+    name: "RYAN_PAGE", 
+    brand: "NXT", 
+    gender: "M"
+  },
+          {
+    name: "HARDBEAR", 
+    brand: "NXT", 
+    gender: "M"
+  },
+          {
+    name: "DADDY_TODD", 
+    brand: "NXT", 
+    gender: "M"
+  },
+          {
+    name: "ANTONIO_DASHING", 
+    brand: "NXT", 
+    gender: "M"
+  },
+          {
+    name: "ANGELO_KNIGHT", 
+    brand: "NXT", 
+    gender: "M"
+  },
+          {
+    name: "ALEX_CONNOR", 
+    brand: "NXT", 
+    gender: "M"
+  },
+          {
+    name: "DANNY_PUMPKIN", 
+    brand: "NXT", 
+    gender: "M"
+  }
+  ];
